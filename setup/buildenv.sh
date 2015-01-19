648e66ff757a35237796303bce1b64d8aad8781c
Michael Kuehl <mickuehl@ratchet.cc>
2015-01-19 17:26:31 +0100

    refactored basic setup

--- setup/buildenv.sh
+++ setup/buildenv.sh
@@ -8,11 +8,11 @@ cd $MAJORDOMUS_ROOT
 source setup/functions.sh # load our functions
 source /etc/majord.conf # load global vars
 
-if [ ! -z "/usr/local/bin/gitreceive"]; then
+if [ ! -L "/usr/local/bin/gitreceive" ]; then
 	sudo ln -s $MAJORDOMUS_ROOT/bin/gitreceive /usr/local/bin/gitreceive
 fi
 
-if [ ! -z "/home/git/receiver"]; then
+if [ ! -d "/home/git" ]; then
 	sudo gitreceive init
 	sudo mkdir -p $MAJORDOMUS_DATA/git
 	sudo chmod 777 $MAJORDOMUS_DATA/git
